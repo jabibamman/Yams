6d32a4062cd2baa478dd71786cbec47a75859fd6
James <james.bidule@gmail.com>
2023-12-21 08:22:16 +0100

    tdd: add first yams logic
    Refactor Yams score method to count the number of 1s and update the score accordingly. If there are exactly 3 1s, set the score to 28.

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -3,7 +3,16 @@ package org.example;
 public class Yams {
     public static int score(int[] des) {
         int score = 0;
+        for (int i = 0; i < des.length; i++) {
+            if (des[i] == 1) {
+                score += 1;
+            }
+        }
 
+        if (score == 3) {
+            score = 28;
+        }
+        
         return score;
     }
 }
