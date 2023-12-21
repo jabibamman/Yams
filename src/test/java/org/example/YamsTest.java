0e664bf954c9aed6293ac33e2bccceea348692c0
James <james.bidule@gmail.com>
2023-12-21 08:24:03 +0100

    add test for carré without impl

--- src/test/java/org/example/YamsTest.java
+++ src/test/java/org/example/YamsTest.java
@@ -5,11 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class YamsTest {
+     // Brelan
      @Test
-        public void should_return_28_when_1_1_1_2_3() {
-            int[] des = {1, 1, 1, 2, 3};
-            int score = Yams.score(des);
-            assertEquals(28, score);
-        }
+     public void should_return_28_when_1_1_1_2_3() {
+         int[] des = {1, 1, 1, 2, 3};
+         int score = Yams.score(des);
+         assertEquals(28, score);
+     }
 
+     // Carré
+    @Test
+    public void should_return_35_when_1_1_1_1_3() {
+        int[] des = {1, 1, 1, 1, 3};
+        int score = Yams.score(des);
+        assertEquals(35, score);
+    }
 }
