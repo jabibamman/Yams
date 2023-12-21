c117766741cd682e1bae493ca384515824d7cb9d
James <james.bidule@gmail.com>
2023-12-21 08:32:08 +0100

    add impl for full

--- src/main/java/org/example/Yams.java
+++ src/main/java/org/example/Yams.java
@@ -3,15 +3,32 @@ package org.example;
 public class Yams {
     public static int score(int[] des) {
         int score = 0;
-        for (int i = 0; i < des.length; i++) {
-            if (des[i] == 1) {
-                score += 1;
+        int[] counts = new int[6]; 
+        for (int die : des) {
+            counts[die - 1]++;
+        }
+
+        boolean brelan = false;
+        boolean paire = false;
+        boolean carre = false;
+
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] == 3) {
+                brelan = true;
+            } else if (counts[i] == 2) {
+                paire = true;
+            } else if (counts[i] == 4) {
+                carre = true;
             }
         }
 
-        if (score == 3) {
+        if (brelan && paire) {
+            score = 30;
+        } else if (brelan) {
             score = 28;
-        } else if (score == 4) {
+        } else if (paire) {
+            score = 0;
+        } else if (carre) {
             score = 35;
         }
 
